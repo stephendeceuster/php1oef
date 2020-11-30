5fa8dbe8c0f52a285aca84e3256b66a4752c7dc8
Stephen De Ceuster <stephen@sidebury.com>
2020-11-30 18:50:01 +0100

    Oefening 2.1 Connecting to database - part II

--- oef2.1/index2.php
+++ oef2.1/index2.php
@@ -3,29 +3,33 @@ error_reporting( E_ALL );
 ini_set( 'display_errors', 1 );
 
 require_once("connection_data.php");
+
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
 
 function getData($sql) {
-    global $conn;
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    global $mysqli;
+
     // Execute query
-    $result = $conn -> query($sql);
+    $result = $mysqli -> query($sql);
 
     // Show result
     if ($result->num_rows > 0) {
         // output elke rij
         while ($row = $result->fetch_assoc()) {
-            $output = '<div class="col-sm-4">';
+            $output = '<div class="col-sm-4 mb-2">';
+            $output .= '<div class="col-sm-12">';
             $output .= '<h3>' . $row["img_title"] . '</h3> ';
             $output .= '<p>' . $row["img_height"] . ' x ' . $row["img_width"] . ' pixels </p>';
             $output .= '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>';
             $output .= '<img src="./img/' . $row["img_filename"] . '" alt="A view from ' . $row["img_title"] . '" title ="A view from ' . $row["img_title"] . '">';
-            $output .= '</div>';
+            $output .= '</div></div>';
             echo $output;
         }
     } else {
@@ -33,9 +37,36 @@ function getData($sql) {
     }
     // Free result set
     $result -> free_result();
+
     // close connection
-    $conn -> close();
+    $mysqli -> close();
+
 }
+
+function getContinents() {
+
+    global $mysqli;
+
+    // Execute query
+    $continents = "SELECT con_naam FROM continents";
+    $result = $mysqli -> query($continents);
+
+    if ($result->num_rows > 0) {
+        // output elke rij
+        while ($row = $result->fetch_assoc()) {
+            $output = '<a class="btn btn-primary" role="button" href="index2.php?continent='. $row["con_naam"] . '" >' . $row["con_naam"] . '</a>';
+            echo $output;
+        }
+    } else {
+        echo "No records found";
+    }
+    // Free result set
+    $result -> free_result();
+
+
+}
+
+
 
 ?>
 
@@ -56,15 +87,20 @@ function getData($sql) {
         .container {
             padding-bottom: 5rem;
         }
-        div.col-sm-4 {
+        div.col-sm-12 {
             transition: all 300ms ease-in-out;
             padding-bottom: 16px;
             border-radius: 4px;
+            margin-bottom: 16px;
         }
-        div.col-sm-4:hover {
-            transform: scale(1.05);
+        div.col-sm-12:hover {
+            transform: scale(1.015);
             box-shadow: 0px 8px 9px -3px rgba(68,68,68,0.61);
             background-color: #eeeeee;
+        }
+        .btn {
+            margin-right: 8px;
+            margin-bottom: 8px;
         }
     </style>
 </head>
@@ -78,9 +114,17 @@ function getData($sql) {
 
 <div class="container">
     <div class="row">
+        <div class="col-12">
+            <?php
+            getContinents();
+            ?>
+        </div>
+    </div>
+    <div class="row">
 
         <?php
-        $selectAll = "SELECT * FROM images WHERE img_continent LIKE '%azie%' ORDER BY img_id";
+
+        $selectAll = 'SELECT * FROM images';
         getData($selectAll);
         ?>
 
@@ -89,3 +133,8 @@ function getData($sql) {
 
 </body>
 </html>
+
+<?php
+// close connection
+$mysqli -> close();
+?>
